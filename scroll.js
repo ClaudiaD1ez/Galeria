3e887eab81dd2b3d1e3cb272c1a958711ad7cf2c
clau <claudia_diez@outlook.com>
2022-09-01 18:49:56 +0200

    scroll

--- scroll.js
+++ scroll.js
@@ -9,6 +9,10 @@ let ps = {
     posicionScroll: 0,
     articulos: document.querySelectorAll("#scroll article"),
     cajaScroll: document.querySelector("#scroll"),
+    botones: document.querySelectorAll("nav ul li a"),
+    ruta: null,
+    intervalo: null,
+    destinoScroll: 0
 }
 
 //////////////////////// METODOS SCROLL
@@ -17,7 +21,12 @@ let ms = {
 
     inicioScroll: function(){
 
-        document.addEventListener("scroll", ms.efectoParallax)
+        document.addEventListener("scroll", ms.efectoParallax);
+
+        for(let i=0; i<ps.botones.length; i++){
+
+            ps.botones[i].addEventListener("click", ms.desplazamiento)
+        }
     },
 
     efectoParallax: function(){
@@ -28,7 +37,7 @@ let ms = {
 
            for(let i=0; i< ps.articulos.length; i++){
            
-              ps.articulos[i].style.marginLeft = "0%";  // todos los articulos se pegan a la izq
+              ps.articulos[i].style.marginLeft = "0%";  // todos los articulos se pegan a la izq.
 
            }
 
@@ -39,6 +48,34 @@ let ms = {
                 ps.articulos[i].style.marginLeft = ps.posicionScroll/15 - 100 +  "%"
             }
         }
+    },
+
+    desplazamiento: function(ruta){
+
+        ruta.preventDefault(); //prevenir eventos po rdefecto
+
+        ps.ruta = ruta.target.getAttribute("href");
+
+        ps.destinoScroll = document.querySelector(ps.ruta).offsetTop;
+
+        ps.intervalo = setInterval(function(){
+
+            if(ps.posicionScroll < ps.destinoScroll){ //si el scroll esta encima del punto al que quiero llegar
+
+                ps.posicionScroll+= 100; // la posición del scroll se va incrementado de a 100 unidades
+            }
+
+            if(ps.posicionScroll >= ps.destinoScroll){
+
+                ps.posicionScroll = ps.destinoScroll; // si no, que tome el destino valor de la posicion de destino
+
+                clearInterval(ps.intervalo) // detener intervalo
+            }
+
+            window.scrollTo(0,ps.posicionScroll) // los parametros son la posicion vertical y horizantal
+
+        })
+
     }
 }
 
